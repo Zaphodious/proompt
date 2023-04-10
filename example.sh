03d7054ed22997d2986bb74f5c34722ca1ab9548
Zaphodious <zaph@zaphodious.com>
2023-04-10 09:50:28 -0800

    Update example.sh to include git functionality

--- example.sh
+++ example.sh
@@ -7,9 +7,17 @@ USTRING="\u@\h"
 
 # Create a function for PROMPT_COMMAND to use
 function proomptme {
-    # In Bash 4.4 and up, PS2 control codes are expandable with @P. Passing
+    # In Bash 4.4 and up, PS1 control codes are expandable with @P. Passing
     # them expanded into proompt will let it properly calculate the size of the text.
-    PS1="$(proompt -i $EUID -c '󰣙' -s f76c59 07102e "${THETIME@P}" -s f04181 07102e " ${USTRING@P}" -s faaa5a 07102e "${WDIR@P}")"
+    PS1="$(proompt -i $EUID \
+    -g "$(git status --porcelain=v2 --branch 2>&1)" \
+    -s f76c59 07102e "${THETIME@P}" \
+    -s f04181 07102e " ${USTRING@P}" \
+    -s faaa5a 07102e "${WDIR@P}" \
+    --git-s committed 4422ff 000000 \
+    --git-s staged 4499ff 000000 \
+    --git-s unstaged ff3399 000000 \
+    )"
 }
 
 # Set the command that will be ran when a prompt is needed. This is
